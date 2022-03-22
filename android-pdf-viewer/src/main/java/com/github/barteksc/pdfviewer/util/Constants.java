1875ed5a546786d030685087f9ce2fdbac31e12d
Sujith Royal <sujithroyal@Sujiths-MacBook-Pro.local>
2022-03-22 17:27:58 +0530

    decrease part size

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/util/Constants.java
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/util/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
      * Tinier : a little bit slower to have the whole page rendered but more reactive.
      * Bigger : user will have to wait longer to have the first visual results
      */
-    public static float PART_SIZE = 600;
+    public static float PART_SIZE = 120;
 
     /** Part of document above and below screen that should be preloaded, in dp */
     public static int PRELOAD_OFFSET = 20;
